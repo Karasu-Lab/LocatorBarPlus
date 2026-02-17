1b5f7bffebc78a175ead2d27423e02a13d8daeb5
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-02-18 01:26:24 +0900

    feat: added submodule

--- modules/KarasunikiLib/src/client/java/com/karasu256/karasunikilib/config/ConfigEvents.java
+++
@@ -1,6 +0,0 @@
-package com.karasu256.karasunikilib.config;
-
-public enum ConfigEvents {
-    CHANGE_FIELD,
-    RESET,
-}
--- modules/KarasunikiLib/src/client/java/com/karasu256/karasunikilib/config/IConfigFieldsAdapter.java
+++
@@ -1,7 +0,0 @@
-package com.karasu256.karasunikilib.config;
-
-import java.util.Map;
-
-public interface IConfigFieldsAdapter {
-    Map<String, FieldTypes> getFields();
-}
